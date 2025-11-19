6da09fa88cf993751458c02a48fa682a01fc2b06
Azis MMZ <mochamad.mmz@bsi.ac.id>
2025-11-19 11:09:34 +0700

    udpate bag making

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,59 +9,20 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 //PRODUCTION LOGIN
-// class AuthController extends Controller
-// {
-//     public function login(Request $request){
-//         if (app()->environment('production')) {
-//             return redirect('https://sso.olefinatifaplas.my.id/login');
-//         } else {
-//             return redirect('http://127.0.0.1:8000/login');
-//         }
-//     }
-
-//     public function postlogin(Request $request)
-//     {
-
-//         $email=$request->email;
-//         $password=$request->password;
-//         $credentials = [
-//             'email' => $email,
-//             'password' => $password
-//         ];
-//         $dologin=Auth::attempt($credentials);
-//         if($dologin){
-//             $checkstatus = User::where('email', $request->email)->first()->is_active;
-//             if($checkstatus == 1){
-//                 return redirect()->route('dashboard')->with('success','Successfully Entered The Application');
-//             } else {
-//                 return redirect()->route('login')->with('fail','Your Account Is Innactive');//jika login gagal dilakukan
-//             }
-//         }
-//         else{
-//             return redirect()->route('login')->with('fail','Wrong Email or Password');
-//         }
-//     }
-
-//     public function logout(Request $request){
-//         Auth::logout();
-//         if (app()->environment('production')) {
-//             return redirect('https://sso.olefinatifaplas.my.id/login');
-//         } else {
-//             return redirect('http://127.0.0.1:8000/login');
-//         }
-//     }
-// }
-
-//DEV
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        return view('auth.login');
+        if (app()->environment('production')) {
+            return redirect('https://sso.olefinatifaplas.my.id/login');
+        } else {
+            return redirect('http://127.0.0.1:8000/login');
+        }
     }
 
     public function postlogin(Request $request)
     {
+
         $email = $request->email;
         $password = $request->password;
         $credentials = [
@@ -74,7 +35,7 @@ class AuthController extends Controller
             if ($checkstatus == 1) {
                 return redirect()->route('dashboard')->with('success', 'Successfully Entered The Application');
             } else {
-                return redirect()->route('login')->with('fail', 'Your Account Is Innactive');
+                return redirect()->route('login')->with('fail', 'Your Account Is Innactive'); //jika login gagal dilakukan
             }
         } else {
             return redirect()->route('login')->with('fail', 'Wrong Email or Password');
@@ -84,6 +45,46 @@ class AuthController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
-        return redirect()->route('login')->with('success', 'Success Logout');
+        if (app()->environment('production')) {
+            return redirect('https://sso.olefinatifaplas.my.id/login');
+        } else {
+            return redirect('http://127.0.0.1:8000/login');
+        }
     }
 }
+
+//DEV
+// class AuthController extends Controller
+// {
+//     public function login(Request $request)
+//     {
+//         return view('auth.login');
+//     }
+
+//     public function postlogin(Request $request)
+//     {
+//         $email = $request->email;
+//         $password = $request->password;
+//         $credentials = [
+//             'email' => $email,
+//             'password' => $password
+//         ];
+//         $dologin = Auth::attempt($credentials);
+//         if ($dologin) {
+//             $checkstatus = User::where('email', $request->email)->first()->is_active;
+//             if ($checkstatus == 1) {
+//                 return redirect()->route('dashboard')->with('success', 'Successfully Entered The Application');
+//             } else {
+//                 return redirect()->route('login')->with('fail', 'Your Account Is Innactive');
+//             }
+//         } else {
+//             return redirect()->route('login')->with('fail', 'Wrong Email or Password');
+//         }
+//     }
+
+//     public function logout(Request $request)
+//     {
+//         Auth::logout();
+//         return redirect()->route('login')->with('success', 'Success Logout');
+//     }
+// }
